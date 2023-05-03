1d0598fe8ffcb46ac7235fa40282282f95fbc2f1
orindojeff <orindojeff@gmail.com>
2023-05-03 06:47:43 +0300

    iniatial commit'

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls.static import static
+from portfolio import settings
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('core.urls')),
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
